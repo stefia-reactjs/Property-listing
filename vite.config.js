2d9929b20829752b1365ae56fe1d223c99608462
Nastya E <dplu99ed@gmail.com>
2025-11-13 00:28:25 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Property-listing/',
+  base: "/Property-listing/",
+  build: { outDir: "docs", emptyOutDir: true },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),                 // <— главный пункт
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
